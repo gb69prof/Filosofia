e42aa823433a1632537726f75730021cab3d5fbe
gb69prof <gb69prof@users.noreply.github.com>
2026-08-02 10:01:10 +0200

    Cache Studio path in PWA

--- Platone-Aristotele/sw.js
+++ Platone-Aristotele/sw.js
@@ -1,5 +1,5 @@
-const CACHE='platone-aristotele-v3';
-const CORE=['./','./index.html','./manifest.webmanifest','./assets/icon.svg','./assets/copertina.PNG','./scopro/','./scopro/index.html'];
+const CACHE='platone-aristotele-v4';
+const CORE=['./','./index.html','./manifest.webmanifest','./assets/icon.svg','./assets/copertina.PNG','./scopro/','./scopro/index.html','./studio/','./studio/index.html'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;if(event.request.mode==='navigate'){event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))));return}event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}))) });
